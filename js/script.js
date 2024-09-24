ee822d380caf3e8ea11e2bb7600b4595ef8ecc40
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-09-24 18:17:07 +0300

    Third Section

---
+++ js/script.js
@@ -0,0 +1,137 @@
+// Navbar
+
+const listItems = document.querySelectorAll(".list-item");
+
+const deactivateListItems = () => {
+    listItems.forEach(item => {
+        item.classList.remove("active");
+    });
+}
+
+const handleListItems = (e) => {
+    deactivateListItems();
+    e.target.classList.add("active");
+}
+
+listItems.forEach(item => {
+    item.addEventListener("click", handleListItems);
+});
+
+const furnitureBtn = document.querySelector(".drop-menu-btn");
+const dropDpwnMenu = document.querySelector(".drop-menu");
+
+const handleDropDownMenu = (e) => {
+    dropDpwnMenu.classList.toggle("show-drop-menu");
+}
+
+furnitureBtn.addEventListener("click", handleDropDownMenu);
+
+
+// ____ Third Section ____
+
+const btns = document.querySelectorAll(".btn-sm");
+
+const deactivateBtns = () => {
+    btns.forEach(btn => {
+        btn.classList.remove("btn-sm-active");
+    });
+}
+
+const handleFurnitureBtns = (e) => {
+    deactivateBtns();
+    e.target.classList.add("btn-sm-active");
+}
+
+btns.forEach(btn => {
+    btn.addEventListener("click", handleFurnitureBtns);
+});
+
+
+// Cards
+
+const cardsContainer = document.querySelector(".card-wrapper");
+
+fetch("data/chair.json").then((request) => {
+    if(!request.ok) {
+        console.log("Something went wrong!");
+        return null;
+    }
+    return request.json();
+}).then(data => {
+    console.log(data);
+    data.forEach(card => {
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+        cardDiv.classList.add("swiper-slide");
+
+        const imageBg = document.createElement("div");
+        imageBg.classList.add("image-bg");
+        imageBg.innerHTML = `<img src="${card.img}" alt="${card.imgAlt}">`;
+
+        cardDiv.appendChild(imageBg);
+
+        const cardBottom = document.createElement("div");
+        cardBottom.classList.add("card-bottom");
+
+        const cardInfo = document.createElement("div");
+        cardInfo.classList.add("card-info");
+        cardInfo.innerHTML = `
+            <p>${card.type}</p>
+            <p class="item-name">${card.name}</p>
+        `;
+
+        const ratingDiv = document.createElement("div");
+        ratingDiv.classList.add("rating");
+
+        for (let i = 0; i < Number(card.rating); i++) {
+            const icon = document.createElement("i");
+            icon.classList.add("fa-solid");
+            icon.classList.add("fa-star");
+            ratingDiv.appendChild(icon);
+        }
+        cardInfo.appendChild(ratingDiv);
+
+        const cardPriceDiv = document.createElement("div");
+        cardPriceDiv.classList.add("card-price");
+
+        const priceDiv = document.createElement("div");
+        priceDiv.classList.add("price");
+        priceDiv.innerHTML = `
+            <p>$</p>
+            <p>${card.price}</p>
+        `;
+
+        const addBtn = document.createElement("button");
+        addBtn.classList.add("btn-dark");
+        addBtn.classList.add("add-btn");
+
+        cardPriceDiv.appendChild(priceDiv);
+        cardPriceDiv.appendChild(addBtn);
+
+        cardBottom.appendChild(cardInfo);
+        cardBottom.appendChild(cardPriceDiv);
+
+        cardDiv.appendChild(cardBottom);
+
+        cardsContainer.appendChild(cardDiv);
+    })
+});
+
+// Swiper
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 50,
+    loop: false,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
